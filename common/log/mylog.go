f04c2b55e05a6f215bd60e1aa1f55979f83c0bf3
scottk.zhang <scottk.zhang@shopee.com>
2022-04-01 21:37:52 +0800

    update

--- common/log/mylog.go
+++ common/log/mylog.go
@@ -12,12 +12,14 @@ var Log *logrus.Logger
 
 func Init() error {
 	Log = logrus.New()
-	fileName := "./log/" + time.Now().Format("2006-01-02") + ".log"
+	fileName := "./log/" + time.Now().Format("2006-01-02 15:04:05") + ".log"
 	writer, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0755)
 	if err != nil {
 		return err
 	}
+	Log.SetLevel(logrus.DebugLevel)
 	Log.SetOutput(io.Writer(writer))
-	Log.SetFormatter(&logrus.JSONFormatter{})
+	//Log.SetFormatter(&logrus.JSONFormatter{})
+	Log.SetFormatter(&logrus.TextFormatter{DisableColors: false})
 	return nil
 }
